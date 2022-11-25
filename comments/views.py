1df4ea0e3b3a8eb51514a4871b8bb059d8eb53c3
DanTaylor28 <dan.taylor1493@gmail.com>
2022-11-25 08:48:43 +0000

    Create LikeList and LikeDetail views and urls to create, list and delete likes if you own them

--- comments/views.py
+++ comments/views.py
@@ -5,6 +5,9 @@ from .serializers import CommentSerializer, CommentDetailSerializer
 
 
 class CommentList(generics.ListCreateAPIView):
+    """
+    List comments or create one if you are logged in.
+    """
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Comment.objects.all()
@@ -14,6 +17,9 @@ class CommentList(generics.ListCreateAPIView):
 
 
 class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a comment, or update or delete it by id if you own it.
+    """
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = CommentDetailSerializer
     queryset = Comment.objects.all()
