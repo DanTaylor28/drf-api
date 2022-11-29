aa9e8376c1b63f7a8a1be543e6ecf7f5473df0a1
DanTaylor28 <dan.taylor1493@gmail.com>
2022-11-29 17:13:05 +0000

    Create followerlist and followerdetail views

--- followers/serializers.py
+++ followers/serializers.py
@@ -19,7 +19,7 @@ class FollowerSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         try:
-            return super.create(validated_data)
+            return super().create(validated_data)
         except IntegrityError:
             raise serializers.ValidationError({
                 'detail': 'possible duplicate'
