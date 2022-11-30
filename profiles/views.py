c457bcb5853e7d808cf534b944059bc8dbb0e9c9
DanTaylor28 <dan.taylor1493@gmail.com>
2022-11-30 10:31:55 +0000

    Add annotation to profiles views to add extra extra fields andfilter search functionality

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
+from django.db.models import Count
 from django.http import Http404
-from rest_framework import status, generics
+from rest_framework import status, generics, filters
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -22,7 +23,21 @@ class ProfileList(generics.ListAPIView):
     No create view as profile creation is handled by django signals.
     """
     serializer_class = ProfileSerializer
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'posts_count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_at',
+        'owner__followed__created_at'
+    ]
 
 
 # class ProfileDetail(APIView):
@@ -60,4 +75,8 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
     """
     serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
